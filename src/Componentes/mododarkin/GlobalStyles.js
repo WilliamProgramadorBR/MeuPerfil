8d376d3597d523634bcb772b04ef5cf926e60145
William/Back-end <108165232+WilliamProgramadorBR@users.noreply.github.com>
2024-01-03 13:06:30 -0300

    Update GlobalStyles.js

--- src/Componentes/mododarkin/GlobalStyles.js
+++ src/Componentes/mododarkin/GlobalStyles.js
@@ -5,7 +5,7 @@ export const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background-color: ${({ theme }) => theme.background}; /* Adicione isso para definir a cor de fundo do corpo */
+    background-color: ${({ theme }) => theme.background}; 
     color: ${({ theme }) => theme.text};
     font-family: 'Arial', sans-serif;
   }
